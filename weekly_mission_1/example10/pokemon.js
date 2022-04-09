28782d1cfaa4933e668ba548da5feb7e490c89a9
unciafidelis <unciafidelis@github.com>
2022-04-09 13:16:39 -0500

    example10 pokemon.js package.json main.js Realizado

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,9 @@
+export default class MyPokemon {
+    constructor (name) {
+      this.name = name
+    }
+  
+    sayHello () {
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+    }
+  }
