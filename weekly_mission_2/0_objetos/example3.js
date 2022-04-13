9aef61e934f994e28ea688c98c21512312bd380f
unciafidelis <unciafidelis@github.com>
2022-04-13 13:45:32 -0500

    Add: example3.js 0_objetos.js

---
+++ weekly_mission_2/0_objetos/example3.js
@@ -0,0 +1,18 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Alejandro Morgan",
+    age: 2,
+    nicknames: [
+     "El Morgan",
+      "Alex",
+    ],
+    address: {
+      zip_code: "98614",
+      street: "Lomas del consuelo",
+      city: "Zac"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)
+  console.log(myObject3.name)
+  console.log(myObject3["address"])
